fa171fdd0e79f568901332628818f2fec332aeb6
Caroline Conte <carolinecontee@gmail.com>
2024-04-22 14:52:45 +0200

    header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const HeroBanner = () => {
   return (
     <section className="heroBanner">
       <div className="heroContent">
-        <h1>What book you looking for?</h1>
+        <h1>What book you are looking for?</h1>
         <div className="heroSubtitle">
           <h2>Explore our catalog and find your next read.</h2>
           <img id="heroGIF" src="/book-gif.png" alt="" />
