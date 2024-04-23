98b2a507db51e4f9b76e8441732f9d172cabd876
Caroline Conte <carolinecontee@gmail.com>
2024-04-23 15:51:51 +0200

    claer lazy

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,31 @@ import Favoritos from './pages/Favoritos'
 import { ProductDetail } from './pages/ProductDetail'
 import { NotFound } from './pages/NotFound'
 import { StandardPage } from './pages/StandardPage'
-
+import { ThanksPage } from './pages/ThanksPage'
 import { Categories } from './pages/Category'
 import { TrendPage } from './pages/TrendPage'
+import { AcessForm } from './pages/AcessForm'
+import { Checkout } from './pages/Checkout'
 import { SearchPage } from './pages/SearchPage'
-import { Suspense, lazy } from 'react'
-import { Loading } from './components/svg/Loading'
-
-const ThanksPage = lazy(() => import('./pages/ThanksPage'))
-const Checkout = lazy(() => import('./pages/Checkout'))
-const AcessForm = lazy(() => import('./pages/AcessForm'));
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path='/' element={<StandardPage />} >
-            <Route index element={<Home />} />
-            <Route path='myfavorites' element={<Favoritos />} />
-            <Route path='/trendingbooks' element={<TrendPage />} />
-            <Route path='/chekout' element={<Checkout />} />
-            <Route path='/search' element={<SearchPage />} />
-            <Route path='/livros/:id' element={<ProductDetail />} />
-            <Route path='/category/:categorie' element={<Categories />} />
-          </Route>
-          <Route path='/thanks' element={<ThanksPage />} />
-          <Route path='/*' element={<NotFound />} />
-          <Route path='/login' element={<AcessForm />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path='/' element={<StandardPage />} >
+          <Route index element={<Home />} />
+          <Route path='myfavorites' element={<Favoritos />} />
+          <Route path='/trendingbooks' element={<TrendPage />} />
+          <Route path='/chekout' element={<Checkout />} />
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='/livros/:id' element={<ProductDetail />} />
+          <Route path='/category/:categorie' element={<Categories />} />
+        </Route>
+        <Route path='/thanks' element={<ThanksPage />} />
+        <Route path='/*' element={<NotFound />} />
+        <Route path='/login' element={<AcessForm />} />
+      </Routes>
     </BrowserRouter>
   )
 }
